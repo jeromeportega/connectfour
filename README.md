b95baa5b6a2ef8fa985ba1f1ecb8d5b66c095eef
Jerome Ortega <Zerospace@Jeromes-MacBook-Pro.local>
2017-07-27 19:05:19 -0700

    Completed game and added additional styling.

--- README.md
+++ README.md
@@ -1,9 +1,33 @@
-# Connect Four Clone
+# Connect Four Clone - Created By: Jerome Ortega
 ### Initial Thoughts
 
-- Thinking of game logic, I know there are certain objectives that must be achieved.
+Thinking of game logic, I know there are certain objectives that must be achieved:
 1. Make sure the game alternates turns as players place pieces.
 2. Players cannot play a piece if the column is full.
-3. Check if there is a winner every round, using the location of the most recently placed piece for reference.
-4. Make the computer pick a random column and send a piece.
-5. If the board is full without a winner, stop the game and let the player reset.
+3. Make the computer pick a random column that still has space and send a piece.
+4. Check if there is a winner every round.  Check all possible win conditions including horizontal, vertical, and diagonal.
+5. If there is a winner, declare who won and make the game reset using player input.  Must reset the visual board as well as the game board.
+6. Check if the board is full after the computer player moves.  Since the player is going first, the computer will have the final piece drop.
+6. If the board is full without a winner, stop the game and let the player reset by declaring a tie.
+
+##### Additional Notes
+- Instead of hard-coding a potential position for every single piece, I felt the best way would be to construct the DOM over time using JavaScript as pieces are played.
+- I thought that a 2-d array or matrix would be the best structure, since it would make position tracking easier, and disallow going outside of the bounds.
+- Checking win condition should try to limit the amount of iterations or else it could quickly get out of control.  We can use the location of the last piece placed.
+
+### Thoughts on Checking For a win
+1. I felt that since there were so few checks that needed to be done each time, I could use for loops to check for win conditions.
+2. I decided that I would write the algorithm so that if a winner was found, the other possible directions wouldn't have to be checked.
+3. I used a variable to track the number of tokens there were in a row, and I knew I wouldn't have to check the token just placed, because I already knew who laid that token.
+4. After writing the horizontal and vertical checking algorithms, diagonals were a touch more difficult, but the idea was the same.  Check both directions of the diagonal, and count up the number of tokens in a row.
+5. I knew that there was a solution that could use recursion to find the answer, but I find recursion more effective if there are vastly more possibilities that need to be crawled through.  This was so limited, that I thought for loops would offer a little more direct control.
+6. After completing this algorithm, I ran some tests on it by both playing the game itself, and by running test arrays over the math to make sure edge cases were accounted for.
+
+### Thoughts on Finishing the Project
+1. Went after the possibility of a tie, and made a function to check that.
+2. If the final possible piece was played and resulted in both the board being filled and a player winning, there had to be an edge case written in to prevent both conditions being true and showing up to the player.
+3. Ending the game, and allowing the player to restart took a little bit of work, as I needed to find a way to make the grid not clickable after a win or tie condition was met.
+4. Having the computer drop a random piece was simple enough, but I love the fact that in JavaScript, there is no built-in function to get a random integer, so a quick Google search was all I needed to get a random integer.
+5. The biggest challenge in styling the game was getting the pieces to fill from the bottom first, and keep the correct pattern.  I finally found the solution myself after searching for a ton of answers that did not work for me.  All I did was invert the columns.  Since the columns are one-dimensional, they still kept the correct number associated to them.
+6. I wanted to do a bit more with the colors than red and blue, because those primary colors really hurt your eyes when they are right next to each other.
+7. I decided to write a short animation to fade in the restart button and who won for a tiny bit of theatrics.
--- js/gamelogic.js
+++ js/gamelogic.js
@@ -1,26 +1,58 @@
 var gameBoard = document.getElementById("gameBoard");
-var gameBoardArray = [
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0],
-  [0,0,0,0,0,0,0]
-];
+var gameBoardArray = [];
 var turn = 1;
+var width = 7;
+var height = 6;
+var currLoc = [];
 
-for (var i = 0; i < 7; i++) {
-  gameBoard.innerHTML += "<div id='" + i + "' class='gameColumn'></div>";
-}
+// Start the Game
+initGame();
 
-var gameColumns = gameBoard.getElementsByClassName("gameColumn");
+// This function initiliazes the game, creates the gameBoardArray, creates the DOM column elements, and starts the game logic via click events on those columns.
+function initGame() {
 
-for (var i = 0; i < gameColumns.length; i++) {
-  gameColumns[i].addEventListener('click', function() {
+  // Creating the gameBoardArray and filling it with all zeroes representing empty spaces.  I wanted to do this so the game could be scaled larger if needed.
+  for (var i = 0; i < height; i++) {
+    gameBoardArray[i] = [];
+    for (var j = 0; j < width; j++) {
+      gameBoardArray[i].push(0);
+    }
+  }
+  for (var i = 0; i < width; i++) {
+    gameBoard.innerHTML += "<div id='" + i + "' class='gameColumn'></div>";
+  }
+  var gameColumns = gameBoard.getElementsByClassName("gameColumn");
 
-    // The game attempts to place a token where the player or computer selected, then this places a token piece into the DOM for visual representation if successful.
-    if(dropToken(event.target.id)) placeToken(event.target);
-  });
+  // This for loop adds an event listener to each column to see which one the player clicks.
+  for (var i = 0; i < gameColumns.length; i++) {
+    gameColumns[i].addEventListener('click', function() {
+
+      // The game attempts to place a token where the player or computer selected, then this places a token piece into the DOM for visual representation if successful.
+      if(dropToken(Number(event.target.id))) {
+        placePlayerToken(event.target);
+
+        // Check if there is a winner after the player's turn.
+        if(winCheck()) {
+          declareWinner();
+          return;
+        } else {
+          turn = 2;
+          computerTurn();
+
+          //Check if there's a winner after the computer's turn.
+          if(winCheck()) {
+            declareWinner();
+            return;
+          } else {
+            turn = 1;
+          }
+
+          // After both players have taken their moves, make a quick check to make sure the whole board isn't full.
+          tieCheck();
+        }
+      }
+    });
+  }
 }
 
 // This function drops the token into the gameBoardArray where the player selected, or the computer selected.
@@ -33,20 +65,154 @@ function dropToken(col) {
     for (var i = gameBoardArray.length - 1; i > -1; i--) {
       if (gameBoardArray[i][col] === 0) {
         gameBoardArray[i][col] = turn;
+        currLoc = [i, col];
         return true;
       }
     }
   }
 }
 
-// placeToken puts a token piece into the DOM only if a token was placed in gameBoardArray
-function placeToken(e) {
-  if(turn == 1) {
-    e.innerHTML += "<div style='background-color: red' class='gameSquare'><div>";
-    turn = 2;
+// placeToken puts a token piece into the DOM only if a player token was placed in gameBoardArray
+function placePlayerToken(e) {
+  e.innerHTML += "<div class='gameSquare redBack'><div>";
+}
+
+// This function puts finds an empty slot in the gameBoardArray and places a token in the array and the DOM.
+function computerTurn() {
+  var col = 0;
+  var placement;
+  while(true) {
+    col = Math.floor(Math.random() * 7);
+    if(gameBoardArray[0][col] === 0) {
+      dropToken(col);
+      placement = document.getElementById(col);
+      placement.innerHTML += "<div class='gameSquare blueBack'><div>";
+      break;
+    }
   }
-  else {
-    e.innerHTML += "<div style='background-color: blue' class='gameSquare'><div>";
-    turn = 1;
+}
+
+// Check if there was a winning move just made by either the computer or the player from the origin of the dropped piece.  Return a boolean.
+function winCheck() {
+  var winCounter = 1;
+  var row = currLoc[0];
+  var col = currLoc[1];
+  var x;
+  var y;
+
+  // Check vertical lines
+  console.log(currLoc);
+  console.log(gameBoardArray);
+  for (var i = row + 1; i < gameBoardArray.length; i++) {
+    if (gameBoardArray[i][col] == turn) winCounter += 1;
+    else break;
   }
+  for (var i = row - 1; i >= 0; i--) {
+    if (gameBoardArray[i][col] == turn) winCounter += 1;
+    else break;
+  }
+
+  // Don't continue the function if a winner was already found to cut further down on iterations.
+  if (winCounter >= 4) return true;
+  else winCounter = 1;
+
+  // Check horizontal lines
+  for (var i = col + 1; i < gameBoardArray[row].length; i++) {
+    if (gameBoardArray[row][i] == turn) winCounter += 1;
+    else break;
+  }
+  for (var i = col - 1; i >= 0; i--) {
+    if (gameBoardArray[row][i] == turn) winCounter += 1;
+    else break;
+  }
+  if (winCounter >= 4) return true;
+  else winCounter = 1;
+
+  // Check bottom-right to upper-left diagonals
+  x = row + 1;
+  y = col + 1;
+
+  while (x < gameBoardArray.length && y < gameBoardArray[row].length) {
+    if (gameBoardArray[x][y] == turn) {
+      winCounter += 1;
+      x += 1;
+      y += 1;
+    } else {
+      break;
+    }
+  }
+
+  x = row - 1;
+  y = col - 1;
+
+  while (x >= 0 && y >= 0) {
+    if (gameBoardArray[x][y] == turn) {
+      winCounter += 1;
+      x -= 1;
+      y -= 1;
+    } else {
+      break;
+    }
+  }
+
+  if (winCounter >= 4) return true;
+  else winCounter = 1;
+
+  // Check bottom-left to upper-right diagonals
+  x = row - 1;
+  y = col + 1;
+
+  while (x >= 0 && y < gameBoardArray[row].length) {
+    if (gameBoardArray[x][y] == turn) {
+      winCounter += 1;
+      x -= 1;
+      y += 1;
+    } else {
+      break;
+    }
+  }
+
+  x = row + 1;
+  y = col - 1;
+
+  while (x < gameBoardArray.length && y >= 0) {
+    if (gameBoardArray[x][y] == turn) {
+      winCounter += 1;
+      x += 1;
+      y -= 1;
+    } else {
+      break;
+    }
+  }
+
+  if (winCounter >= 4) return true;
+
+  // If no winner is found during the tests, return false.
+  return false;
+}
+
+// Declares a winner by who laid down the winning token.  Let's the user know who won and allows them to restart the game.
+function declareWinner() {
+  if (turn == 1) {
+    gameBoard.innerHTML += "<div class='animated fadeIn'><h1>You Win!</h1><h4>Click the Button to Restart the Game!</h4><br><button id='restart' class='blueBack red'>Restart</button></div>";
+    document.getElementById('restart').addEventListener('click', function() {
+      location.reload();
+    });
+  } else {
+    gameBoard.innerHTML += "<div class='animiated fadeIn'><h1>The Computer Wins!</h1><h4>Click the Button to Restart the Game!</h4><br><button id='restart' class='blueBack red'>Restart</button></div>";
+    document.getElementById('restart').addEventListener('click', function() {
+      location.reload();
+    });
+  }
+}
+
+// Checked if there is a draw by seeing if the entire top row has been filled.  If so, let's the user know it's a tie.
+function tieCheck() {
+  for (var i = 0; i < gameBoardArray[0].length; i++) {
+    if (gameBoardArray[0][i] === 0) return;
+  }
+  gameBoard.innerHTML += "<div class='animted fadeIn'><h1>Its a draw!</h1><h4>Click the Button to Restart the Game!</h4><button id='restart' class='blueBack red'>Restart</button></div>";
+  document.getElementById('restart').addEventListener('click', function() {
+    location.reload();
+  });
 }
